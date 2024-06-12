e995dadb5a2c95150e42d513fd6d3b3a00225298
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-12 14:00:23 +0200

    feat: use postcss for stylesheets

--- lib/kiqr/components/engine.rb
+++ lib/kiqr/components/engine.rb
@@ -3,13 +3,19 @@ module Kiqr
     class Engine < ::Rails::Engine
       config.autoload_paths << Kiqr::Components::Engine.root.join("app", "components")
 
-      initializer "kiqr.configure.view_components" do |app|
+      initializer "kiqr_components.configure.view_components" do |app|
         config.view_component.preview_paths << Kiqr::Components::Engine.root.join("app", "components")
       end
 
-      initializer "kiqr.configure.lookbook" do |app|
+      initializer "kiqr_components.configure.lookbook" do |app|
         app.config.lookbook.project_name = "KIQR Components"
         app.config.lookbook.preview_collection_label = "Components"
+      end
+
+      # Add the app/javascript directory to the asset pipeline
+      initializer "kiqr_components.assets.precompile" do |app|
+        app.config.assets.paths << Kiqr::Components::Engine.root.join("app", "assets", "dist")
+        app.config.assets.precompile += %w[kiqr_components.css kiqr_components.js]
       end
     end
   end
